0211b3a16d45f6cad8d6169d602d61b991fc4265
anthony1572010 <31384895+anthony1572010@users.noreply.github.com>
2018-01-26 16:38:24 +0700

    udh di import butter knife

--- app/src/main/java/com/anthony/androidexpensemanager/RegisterActivity.java
+++ app/src/main/java/com/anthony/androidexpensemanager/RegisterActivity.java
@@ -1,13 +1,60 @@
 package com.anthony.androidexpensemanager;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtName) EditText txtName;
+    @BindView(R.id.txtEmail) EditText txtEmail;
+    @BindView(R.id.txtPassword) EditText txtPassword;
+    @BindView(R.id.txtRePassword) EditText txtRePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnRegister)
+    void btnRegisterAction(){
+        if(!txtName.getText().toString().trim().isEmpty() && !txtEmail.getText().toString().trim().isEmpty() &&
+                !txtPassword.getText().toString().trim().isEmpty() && !txtRePassword.getText().toString().trim().isEmpty()){
+            AlertDialog alertDialog=new AlertDialog.Builder(RegisterActivity.this).create();
+            alertDialog.setTitle("Warning !!!");
+            alertDialog.setMessage("Any field need be filled");
+            alertDialog.show();
+        }
+        else if(txtPassword.getText().toString().trim() != txtRePassword.getText().toString().trim()){
+            AlertDialog alertDialog=new AlertDialog.Builder(RegisterActivity.this).create();
+            alertDialog.setTitle("Warning !!!");
+            alertDialog.setMessage("Password and RePassword must same");
+            alertDialog.show();
+        }
+        else{
+            new AlertDialog.Builder(RegisterActivity.this).setTitle("Confirmation !!!")
+                    .setMessage("Are you sure want to register?")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent moveToLoginActivity=new Intent(RegisterActivity.this,loginActivity.class);startActivity(moveToLoginActivity);
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    }).show();
+        }
     }
 }
--- app/src/main/java/com/anthony/androidexpensemanager/loginActivity.java
+++ app/src/main/java/com/anthony/androidexpensemanager/loginActivity.java
@@ -1,13 +1,53 @@
 package com.anthony.androidexpensemanager;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class loginActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtEmail) EditText txtEmail;
+    @BindView(R.id.txtPassword) EditText txtPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    void btnLoginAction(){
+        if(txtEmail.getText().toString().trim().equals("admin@gmail.com") && txtPassword.getText().toString().trim().equals("admin")){
+            Intent movetoMenuActivity=new Intent(loginActivity.this,MenuActivity.class);
+            startActivity(movetoMenuActivity);
+            System.out.println("masuk if");
+        }
+        else{
+            AlertDialog alertDialog=new AlertDialog.Builder(loginActivity.this).create();
+            alertDialog.setTitle("Warning !!!");
+            alertDialog.setMessage("Wrong email or password");
+            alertDialog.show();
+            System.out.println("masuk else");
+        }
+    }
+
+    @OnClick(R.id.btnRegister)
+    void btnRegisterAction(){
+        Intent moveToRegisterActivity=new Intent(loginActivity.this,RegisterActivity.class);
+        startActivity(moveToRegisterActivity);
+    }
+    @OnClick(R.id.txtForgotPassword)
+    void txtForgotPasswordAction(){
+        System.out.println("txtForgotPasswordAction");
     }
 }
