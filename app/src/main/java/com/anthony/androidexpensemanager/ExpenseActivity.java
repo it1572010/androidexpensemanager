50db906feabe908bf4f7a7720a3809cec4b13950
anthony1572010 <31384895+anthony1572010@users.noreply.github.com>
2018-01-29 12:05:30 +0700

    ky fix UI

--- app/src/main/java/com/anthony/androidexpensemanager/ExpenseActivity.java
+++ app/src/main/java/com/anthony/androidexpensemanager/ExpenseActivity.java
@@ -1,7 +1,10 @@
 package com.anthony.androidexpensemanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.OnClick;
 
 public class ExpenseActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class ExpenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense);
+    }
+
+    @OnClick(R.id.btnSubmit)
+    void btnSubmitAction(){
+        Intent moveToMenuActivity=new Intent(ExpenseActivity.this,MenuActivity.class);
+        startActivity(moveToMenuActivity);
     }
 }
--- app/src/main/java/com/anthony/androidexpensemanager/IncomeActivity.java
+++ app/src/main/java/com/anthony/androidexpensemanager/IncomeActivity.java
@@ -1,7 +1,10 @@
 package com.anthony.androidexpensemanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.OnClick;
 
 public class IncomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class IncomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income);
+    }
+
+    @OnClick(R.id.btnSubmit)
+    void btnSubmitAction(){
+        Intent moveToMenuActivity=new Intent(IncomeActivity.this,MenuActivity.class);
+        startActivity(moveToMenuActivity);
     }
 }
