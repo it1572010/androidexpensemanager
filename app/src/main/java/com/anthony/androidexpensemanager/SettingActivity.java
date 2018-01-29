3fdf3121fe93ef34c8cbd4793a35915d723e13a1
anthony1572010 <31384895+anthony1572010@users.noreply.github.com>
2018-01-29 20:44:25 +0700

    udh di tambahin intent

--- app/src/main/java/com/anthony/androidexpensemanager/SettingActivity.java
+++ app/src/main/java/com/anthony/androidexpensemanager/SettingActivity.java
@@ -1,7 +1,10 @@
 package com.anthony.androidexpensemanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.OnClick;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+    }
+
+    @OnClick(R.id.btnSubmit)
+    void btnSubmitAction(){
+        Intent moveToMenuActivity=new Intent(SettingActivity.this,MenuActivity.class);
+        startActivity(moveToMenuActivity);
     }
 }
