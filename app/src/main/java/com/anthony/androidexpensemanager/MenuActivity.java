87bb387b579a427b7c73351932b2955ab86d21ad
anthony1572010 <31384895+anthony1572010@users.noreply.github.com>
2018-01-29 07:25:08 +0700

    penambahan expense

--- app/src/main/java/com/anthony/androidexpensemanager/MenuActivity.java
+++ app/src/main/java/com/anthony/androidexpensemanager/MenuActivity.java
@@ -1,7 +1,11 @@
 package com.anthony.androidexpensemanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+
+import butterknife.OnClick;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    @OnClick(R.id.btnNewIncome)
+    void btnNewIncomeAction(){
+        Intent moveToNewIncomeActivity=new Intent(MenuActivity.this,IncomeActivity.class);
+        startActivity(moveToNewIncomeActivity);
+    }
+
+    @OnClick(R.id.btnNewExpense)
+    void btnNewExpense(){
+        Intent moveToNewExpenseActivity=new Intent(MenuActivity.this,ExpenseActivity.class);
+        startActivity(moveToNewExpenseActivity);
     }
 }
