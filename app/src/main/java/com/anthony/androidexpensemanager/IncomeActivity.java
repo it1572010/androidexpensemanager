88a9a2cf1ab6670173cd6d707456bdca785667ba
anthony1572010 <31384895+anthony1572010@users.noreply.github.com>
2018-02-02 14:09:39 +0700

    pop up calendar

--- app/src/main/java/com/anthony/androidexpensemanager/IncomeActivity.java
+++ app/src/main/java/com/anthony/androidexpensemanager/IncomeActivity.java
@@ -1,13 +1,25 @@
 package com.anthony.androidexpensemanager;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class IncomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtTime)
+    EditText txtTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,27 @@ public class IncomeActivity extends AppCompatActivity {
     void btnSubmitAction(){
         Intent moveToMenuActivity=new Intent(IncomeActivity.this,MenuActivity.class);
         startActivity(moveToMenuActivity);
+    }
+
+    Calendar calendar=Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener date=new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            calendar.set(Calendar.YEAR,year);
+            calendar.set(Calendar.MONTH,monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+            updateTxtTime();
+        }
+    };
+
+    private void updateTxtTime(){
+        String format = "MM/DD/YYYY";
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat(format, Locale.US);
+        txtTime.setText(simpleDateFormat.format(calendar.getTime()));
+    }
+
+    public void onClickTxtTime(View v){
+        new DatePickerDialog(this,date,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
